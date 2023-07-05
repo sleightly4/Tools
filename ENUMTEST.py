75436d43a22c7101e6ba559e0b3fa3f79a49f3aa
sleightly4 <138668059+sleightly4@users.noreply.github.com>
2023-07-05 04:48:20 -0500

    Create ENUMTEST.py

---
+++ ENUMTEST.py
@@ -0,0 +1,31 @@
+import subprocess
+import os
+
+# Check SUIDs
+with open(os.devnull, 'w') as devnull:
+    suids = subprocess.check_output(['find', '/', '-perm', '-4000'], stderr=devnull)
+suids = suids.decode().strip().split('\n')
+print('SUIDs:')
+for suid in suids:
+    print(suid)
+
+# Check GUIDs
+with open(os.devnull, 'w') as devnull:
+    guids = subprocess.check_output(['find', '/', '-perm', '-2000'], stderr=devnull)
+guids = guids.decode().strip().split('\n')
+print('\nGUIDs:')
+for guid in guids:
+    print(guid)
+
+# Search files for specific keywords
+keywords = ['password', 'username', 'user', 'pass']
+print('\nFiles containing the keywords:')
+for keyword in keywords:
+    try:
+        with open(os.devnull, 'w') as devnull:
+            grep_output = subprocess.check_output(['grep', '-r', '-w', keyword, '/home', '/root'], stderr=devnull)
+        grep_output = grep_output.decode().strip().split('\n')
+        for line in grep_output:
+            print(line)
+    except subprocess.CalledProcessError:
+        pass
